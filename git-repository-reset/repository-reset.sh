922760c2554965869f26993a59fba8e9327995fb
Alessandro Rosa <alessandro.rosa@intre.it>
2024-02-05 16:57:17 +0100

    Adding Git Repository Reset

---
+++ git-repository-reset/repository-reset.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Enter username:"
+read username
+echo "Enter password:"
+read -s password
+
+remote=$(git remote get-url origin)
+auth_remote=${remote/https:\/\//https:\/\/${username}:${password}@}
+
+git fetch $auth_remote
+
+branches=$(git branch -r)
+branch=$(git branch -r| head -n 1 | awk -F'/' '{print $NF}')
+if [[ $branches == *"master"* ]]; then
+	branch="master"
+fi
+if [[ $branches == *"develop"* ]]; then
+	branch="develop"
+fi
+
+git checkout "$branch"
+git pull $auth_remote
+git stash clear
+branches=$(git branch | grep -v "$branch")
+for branch in $branches
+do
+   git branch -D "$branch"
+done
+
+echo "Git repository restored"
