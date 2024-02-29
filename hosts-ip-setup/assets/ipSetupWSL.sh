6f5d2a93e1de806d9221aeb50e18f0d37af7e773
Alessandro Rosa <alessandro.rosa@intre.it>
2024-02-29 09:38:18 +0100

    using host path variable instead hard coded path WSL host file

--- hosts-ip-setup/assets/ipSetupWSL.sh
+++ hosts-ip-setup/assets/ipSetupWSL.sh
@@ -2,6 +2,6 @@
 HOST_PATH=$1
 HOST_LABEL=$2
 IP=$3
-OLD_IP_LIN=$(grep -P "^.*$HOST_LABEL" /etc/hosts | awk '{print $1}')
+OLD_IP_LIN=$(grep -P "^.*$HOST_LABEL" $HOST_PATH | awk '{print $1}')
 echo "Old $HOST_LABEL IP in WSL: $OLD_IP_LIN"
-sed -i "s/$OLD_IP_LIN/$IP/g" /etc/hosts
+sed -i "s/$OLD_IP_LIN/$IP/g" $HOST_PATH
